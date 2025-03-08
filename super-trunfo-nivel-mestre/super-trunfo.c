724b1b36a350af292e9ecfc358dcc141cbd9880f
AlexOSilva <alexoliveirasilva353@gmail.com>
2025-03-08 15:47:37 +0000

    Att1

--- super-trunfo-nivel-mestre/super-trunfo.c
+++ super-trunfo-nivel-mestre/super-trunfo.c
@@ -25,15 +25,15 @@ printf("Digite o código(ex:A01): ") ;
 scanf("%s" , codigo1) ;
 
 printf("Digite o nome da cidade(ex: Manaus): ");
-scanf("%s" , cidade1) ;
+scanf(" %s" , cidade1) ;
 
 printf("Digite a população (ex:50000) cinquenta mil: "); 
-scanf("%d" , &populacao1) ;
+scanf(" %d" , &populacao1) ;
 
 printf("Digite os números de pontos turísticos (ex: 10): ");
 scanf("%d" , &pontosturisticos1) ; 
 
-printf("Digite o Pib(ex: 700.28) milhões: "); 
+printf("Digite o Pib(ex: 700.28) bilhões: "); 
 scanf("%f", &pib1); 
 
 printf("Digite a área(ex:1521.11) km²: ") ;
@@ -46,7 +46,7 @@ printf("Código: %s\n " , codigo1) ;
 printf("Cidade: %s\n" , cidade1) ;
 printf("População: %d\n" , populacao1);
 printf("Pontos Turísticos: %d\n" , pontosturisticos1);
-printf("PIB: %.2f\n milhões de reais\n" , pib1) ;
+printf("PIB: %.2f\n bilhões de reais\n" , pib1) ;
 printf("Àrea Km2: %.2f\n" , area1) ;
 
 printf("\nSuper Trunfo - Países\n");
@@ -67,7 +67,7 @@ scanf("%d" , &populacao2) ;
 printf("Digite os números de pontos turísticos (ex: 10): ");
 scanf("%d" , &pontosturisticos2) ; 
 
-printf("Digite o Pib(ex: 700.28) milhões: "); 
+printf("Digite o Pib(ex: 700.28) bilhões: "); 
 scanf("%f", &pib2); 
 
 printf("Digite a área(ex:1521.11) km²: ") ;
@@ -80,34 +80,38 @@ printf("Código:%s\n " , codigo2) ;
 printf("Cidade: %s\n" , cidade2) ;
 printf("População: %d\n" , populacao2);
 printf("Pontos Turísticos: %d\n" , pontosturisticos2);
-printf("PIB: %.2f\n milhões de reais\n" , pib2) ;
+printf("PIB: %.2f\n bilhões de reais\n" , pib2) ;
 printf("Àrea Km2: %.2f\n" , area2) ;
 
 //Calcular a Densidade Populacional Carta 1
 printf("\n***Cáculo da Densidade populacional da carta 1***\n");
-float densidade1 = (float) (populacao1 / area1);
-printf("A Densidade populacional da carta 1 é : %.2f\n" , densidade1);
+float densidade1 = (float) populacao1 / area1;
+printf("\nA Densidade populacional da carta 1 é : %.2f\n" , densidade1);
 
 //Calcular o PIB per Capita Carta 1
 printf("\n***Cáculo do PIB per Capita da carta 1***\n");
-float pibcap1= (float) (pib1 / populacao1);
-printf("O PIB per Capita da carta 1 é : %.2f\n reais" , pibcap1);
+float pibcap1= (float) (pib1* 1e9) / populacao1;
+printf("\nO PIB per Capita da carta 1 é : %.2f\n reais" , pibcap1);
 
 //Calcular a Densidade Populacional Carta 2
 printf("\n***Cáculo da Densidade populacional da carta 2***\n");
-float densidade2 = (float) (populacao2 / area2);
-printf("A Densidade populacional da carta 2 é : %.2f\n" , densidade2);
+float densidade2 = (float) populacao2 / area2;
+printf("\nA Densidade populacional da carta 2 é : %.2f\n" , densidade2);
 
 //Calcular o PIB per Capita Carta 2
 printf("\n***Cáculo do PIB per Capita da carta 2***\n");
-float pibcap2 = (float) (pib2/populacao2);
-printf("O PIB per Capita da carta 2 é : %.2f\n reais" , pibcap2);
+float pibcap2 = (float) (pib2* 1e9)/populacao2;
+printf("\nO PIB per Capita da carta 2 é : %.2f\n reais" , pibcap2);
 
 //Calcular o Super Poder
 
 printf("\n***Cáculo do Super Poder da Carta 1***\n");
-float superpoder1 = populacao1 + area1 + pib1 + pontosturisticos1 + pibcap1 + (1.0f / densidade1); )
-printf(" O poder da Carta 1 é %.2f"\n , superpoder1);
+float superpoder1 = populacao1 + area1 + pib1 + pontosturisticos1 + pibcap1 + (1.0f / densidade1);
+printf(" O poder da Carta 1 é %.2f\n" , superpoder1);
+
+printf("\n***Cáculo do Super Poder da Carta 2***\n");
+float superpoder2 = populacao2 + area2 + pib2 + pontosturisticos2 + pibcap2 + (1.0f / densidade1);
+printf(" O poder da Carta 2 é %.2f\n" , superpoder2);
 
 
 
